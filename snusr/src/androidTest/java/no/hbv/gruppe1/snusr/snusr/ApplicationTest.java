189dce7fe6c2c9eb4d6b6d36b7d345412b4b232c
Khest <knut.j.hesten@student.hbv.no>
2016-04-10 15:44:43 +0200

    lolol

---
+++ snusr/src/androidTest/java/no/hbv/gruppe1/snusr/snusr/ApplicationTest.java
@@ -0,0 +1,13 @@
+package no.hbv.gruppe1.snusr.snusr;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
