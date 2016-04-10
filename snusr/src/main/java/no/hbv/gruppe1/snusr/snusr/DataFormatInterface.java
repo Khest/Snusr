bb1950904392e664d4bf85691eb60b758536a66e
Khest <knut.j.hesten@student.hbv.no>
2016-04-10 19:25:39 +0200

    Stubs for dataformat and imagehandler interfaces

---
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/DataFormatInterface.java
@@ -0,0 +1,12 @@
+package no.hbv.gruppe1.snusr.snusr;
+
+/**
+ * Created by Dakh on 2016-04-10.
+ */
+public interface DataFormatInterface {
+    //Parent class that facilitates date, string and currency formatting.
+    //Exists between GUI and database
+
+
+    String toString();
+}
---
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/ImageHandlerInterface.java
@@ -0,0 +1,11 @@
+package no.hbv.gruppe1.snusr.snusr;
+
+/**
+ * Created by Dakh on 2016-04-10.
+ */
+public interface ImageHandlerInterface {
+
+    Object handleInputStream(Object input);
+    Object cropImage(Object input);
+    Object resizeImage(Object input);
+}
