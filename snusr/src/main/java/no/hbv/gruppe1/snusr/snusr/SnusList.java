47dd9cddc4b7b13c3bed6d183bd2c4a8d5363945
hakonst <hakonst@hotmail.no>
2016-04-10 20:29:42 +0200

    Fragment for adding snus added. Not sure if needed?

--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/SnusList.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/SnusList.java
@@ -9,14 +9,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class SnusList extends Fragment {
 
-    public MainActivityFragment() {
+    public SnusList() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_snuslist, container, false);
     }
 }
