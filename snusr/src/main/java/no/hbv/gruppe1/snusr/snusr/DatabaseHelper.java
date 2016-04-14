638e204442c2a21d3b50caa27a23e7c5505b1532
hakonst <hakonst@hotmail.no>
2016-04-14 12:43:34 +0200

    Lagt til metoder for å legge inn data i tabellene.

--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/DatabaseHelper.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package no.hbv.gruppe1.snusr.snusr;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -16,6 +18,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TEXT = "TEXT";
     public static final String CREATE_TABLE_SNUS = "CREATE TABLE " + FeedEntry.DATABASE_TABLE_SNUS + "("
             + FeedEntry.col_snus_id + " INTEGER PRIMARY KEY, "
+            + FeedEntry.col_snus_name + " " + TEXT + ", "
             + FeedEntry.col_snus_manufactorer + " " + INTEGER + ", "
             + FeedEntry.col_snus_line + " " + INTEGER + ", "
             + FeedEntry.col_snus_taste1 + " " + INTEGER + ", "
@@ -127,10 +130,67 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(CREATE_TABLE_LINE);
         db.execSQL(CREATE_TABLE_TYPE);
         db.execSQL(CREATE_TABLE_TASTE);
+        db.execSQL(CREATE_TABLE_SNUS);
+        db.execSQL(CREATE_TABLE_MYLIST);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onCreate(db);
+    }
+
+    public boolean putDummyData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return true;
+    }
+
+    public void putManufactorer(SQLiteDatabase db, String name, String url, String country){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_manufactorer_name, name);
+        input.put(FeedEntry.col_manufactorer_url, url);
+        input.put(FeedEntry.col_manufactorer_country, country);
+        db.insert(FeedEntry.DATABASE_TABLE_MANUFACTORER, null, input);
+    }
+
+    public void putLine(SQLiteDatabase db, int manufactorer, String name){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_line_manufactorer, manufactorer);
+        input.put(FeedEntry.col_line_name, name);
+        db.insert(FeedEntry.DATABASE_TABLE_LINE, null, input);
+    }
+
+    public void putType(SQLiteDatabase db, String text){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_type_text, text);
+        db.insert(FeedEntry.DATABASE_TABLE_TYPE, null, input);
+    }
+
+    public void putTaste(SQLiteDatabase db, String taste){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_taste_taste, taste);
+        db.insert(FeedEntry.DATABASE_TABLE_TASTE, null, input);
+    }
+
+    public void putSnus(SQLiteDatabase db, String name, int manufactorer, int line, int taste1, int taste2, int taste3,
+                        double strength, double nicotinelevel, double rank, int type){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_snus_name, name);
+        input.put(FeedEntry.col_snus_manufactorer, manufactorer);
+        input.put(FeedEntry.col_snus_line, line);
+        input.put(FeedEntry.col_snus_taste1, taste1);
+        input.put(FeedEntry.col_snus_taste2, taste2);
+        input.put(FeedEntry.col_snus_taste3, taste3);
+        input.put(FeedEntry.col_snus_strength, strength);
+        input.put(FeedEntry.col_snus_nicotinelevel, nicotinelevel);
+        input.put(FeedEntry.col_snus_totalrank, rank);
+        input.put(FeedEntry.col_snus_type, type);
+        db.insert(FeedEntry.DATABASE_TABLE_SNUS, null, input);
+    }
+
+    public void putMyList(SQLiteDatabase db, int snus, int rank){
+        ContentValues input = new ContentValues();
+        input.put(FeedEntry.col_mylist_snusid, snus);
+        input.put(FeedEntry.col_mylist_myrank, rank);
+        db.insert(FeedEntry.DATABASE_TABLE_MYLIST, null, input);
     }
 }
