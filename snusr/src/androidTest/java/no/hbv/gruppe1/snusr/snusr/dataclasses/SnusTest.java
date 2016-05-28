cf9413c6b5b8a8e5097700a408edb984655131f0
Khest <knut.j.hesten@student.hbv.no>
2016-05-28 13:15:09 +0200

    Test class for static Snus datakeeper

---
+++ snusr/src/androidTest/java/no/hbv/gruppe1/snusr/snusr/dataclasses/SnusTest.java
@@ -0,0 +1,53 @@
+package no.hbv.gruppe1.snusr.snusr.dataclasses;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+import android.util.Log;
+import no.hbv.gruppe1.snusr.snusr.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dakh on 2016-05-28.
+ */
+public class SnusTest extends AndroidTestCase {
+    public static final String TAG = "SnusrDebug";
+    private GetSnusDB getSnusDB;
+    private RenamingDelegatingContext context;
+    private DatabaseHelper db;
+
+    public void setUp() {
+        context = new RenamingDelegatingContext(getContext(), "test_");
+        db = new DatabaseHelper(context);
+        db.putDummyData();
+        getSnusDB = new GetSnusDB();
+    }
+
+    @SuppressWarnings("Duplicates")
+    public void testSnusExists() throws Exception {
+        List<Filtration> filtrationList = new ArrayList<>();
+        Filtration f1 = Filtration.MANUFACTURER;
+        Filtration f2 = Filtration.LINE;
+        Filtration f3 = Filtration.NAME;
+        String manufacturer = "swedish match";
+        String line = "G3";
+        String name = "extra strong";
+        f1.setSearchValue(manufacturer);
+        f2.setSearchValue(line);
+        f3.setSearchValue(name);
+        filtrationList.add(f1);
+        filtrationList.add(f2);
+        filtrationList.add(f3);
+        Cursor c = getSnusDB.fetchSnus(context, filtrationList, null);
+        Log.d(TAG, DatabaseUtils.dumpCursorToString(c));
+    }
+
+    public void tearDown() throws Exception {
+        db.close();
+        super.tearDown();
+    }
+}
