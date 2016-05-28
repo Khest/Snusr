bfe0844dafa1f052cb3ff078e13a1612f05044df
Khest <knut.j.hesten@student.hbv.no>
2016-05-28 13:14:31 +0200

    Refactored Snus class to a general data validation and database insert class Sets and validates values Check whether snus exists in database or not Inserts new snus into database Globals class handles static variables

---
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/dataclasses/Globals.java
@@ -0,0 +1,14 @@
+package no.hbv.gruppe1.snusr.snusr.dataclasses;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Knut Johan Hesten 2016-05-28.
+ */
+public class Globals {
+    public static final String TAG = "SnusrDebug";
+
+    public static final int IMAGE_QUALITY = 100;
+    public static final Bitmap.CompressFormat COMPRESS_FORMAT = Bitmap.CompressFormat.JPEG;
+
+}
