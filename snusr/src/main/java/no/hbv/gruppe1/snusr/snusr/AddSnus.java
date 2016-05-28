e4430070bef642340e1ecf4be604a1fefa98414b
MLongz <long.8l1a@gmail.com>
2016-05-28 17:54:59 +0200

    Bluetooth og addsnus ikke ferdig

--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/AddSnus.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/AddSnus.java
@@ -1,9 +1,12 @@
 package no.hbv.gruppe1.snusr.snusr;
 
-import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,72 +15,38 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddSnus.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddSnus#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddSnus extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
     RatingBar rating;
     ImageView snusImage;
-    EditText innName, innManu;
+    EditText innName, innManu, innLine;
     Button btnAdd, btnAddSnusImage;
     Spinner spinSweet, spinSalt, spinTaste, spinStr;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddSnus.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddSnus newInstance(String param1, String param2) {
-        AddSnus fragment = new AddSnus();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 111;
+    int SELECT_IMAGE = 222;
 
-    public AddSnus() {
-        // Required empty public constructor
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Uri fileUri;
+    File file;
+
+    Context context;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v =  inflater.inflate(R.layout.fragment_add_snus, container, false);
+        final View v = inflater.inflate(R.layout.fragment_add_snus, container, false);
+        context = getActivity();
         snusImage = (ImageView) v.findViewById(R.id.addSnus_imgview);
         rating = (RatingBar) v.findViewById(R.id.ratingbar_addsnus);
         innName = (EditText) v.findViewById(R.id.etxt_name);
         innManu = (EditText) v.findViewById(R.id.etxt_manu);
+        innLine = (EditText) v.findViewById(R.id.etxt_line);
         btnAdd = (Button) v.findViewById(R.id.btn_addsnus);
         btnAddSnusImage = (Button) v.findViewById(R.id.btn_addSnusImage);
         spinSalt = (Spinner) v.findViewById(R.id.spin_salt);
@@ -85,50 +54,78 @@ public class AddSnus extends Fragment {
         spinStr = (Spinner) v.findViewById(R.id.spin_str);
         spinTaste = (Spinner) v.findViewById(R.id.spin_taste);
 
+        btnAddSnusImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+                //openGallery();
+            }
+        });
         return v;
     }
 
+    public void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),SELECT_IMAGE);
+    }
+
+    public void takePicture() {
+        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); //handle camera and return image
+//        CameraHandler cHandler = new CameraHandler();
+//        file = cHandler.createImageFile();
+//        fileUri = Uri.fromFile(file);
+//        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if(resultCode == getActivity().RESULT_OK){
+                if(data != null){
+                    // Image captured and saved to fileUri specified in the Intent
+                    //Bitmap bitmap = CameraHandler.decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    //innName.setText(data.getData().toString());
+                    snusImage.setImageBitmap(bitmap);
+                }
+            }
+        }
+        if(requestCode == SELECT_IMAGE){
+            if(resultCode == getActivity().RESULT_OK){
+                if (data != null) {
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+                        snusImage.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (resultCode == getActivity().RESULT_CANCELED) {
+                    Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     public void addSnus(){
+        String mName, mManu, mSweet, mSalt, mTaste, mStr, mRating, mLine;
+
+        mName = innName.getText().toString();
+        mManu = innManu.getText().toString();
+        mLine = innLine.getText().toString();
+        mSalt = spinSalt.getSelectedItem().toString();
+        mSweet = spinSweet.getSelectedItem().toString();
+        mStr = spinStr.getSelectedItem().toString();
+        mTaste = spinTaste.getSelectedItem().toString();
+
 
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
 
 }
