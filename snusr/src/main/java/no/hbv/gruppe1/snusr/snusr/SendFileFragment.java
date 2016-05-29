a9e3c4fe29381d5c0fc5c14876f41a0ed50ba623
Khest <knut.j.hesten@student.hbv.no>
2016-05-29 16:21:54 +0200

    Bluetooth connectivity Handles client/server and sends testdata between devices

--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/SendFileFragment.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/SendFileFragment.java
@@ -1,5 +1,6 @@
 package no.hbv.gruppe1.snusr.snusr;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -8,23 +9,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-import no.hbv.gruppe1.snusr.snusr.dataclasses.BluetoothHandler;
+import android.widget.*;
 import no.hbv.gruppe1.snusr.snusr.dataclasses.BluetoothHandler2;
 import no.hbv.gruppe1.snusr.snusr.dataclasses.Globals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 /**
  * Created by Long Huynh on 27.05.2016.
@@ -34,13 +31,16 @@ public class SendFileFragment extends Fragment {
     private ListView listDevicesView;
     private List<BluetoothDevice> listDevices;
     Button btn_send, btn_search;
-    BluetoothHandler2 bHandler = null;
+    BluetoothHandler2 bluetoothHandler = null;
     BluetoothAdapter mBluetoothAdapter = null;
     ArrayAdapter<String> adapter;
 
+
     //UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    private static final int REQUEST_CONNECTION = 1;
     private final int REQUEST_ENABLE_BT = 2;
+
 
     // Create a BroadcastReceiver for ACTION_FOUND
     final BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -57,12 +57,61 @@ public class SendFileFragment extends Fragment {
         }
     };
 
+    private final Handler messageHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Activity fragmentActivity = getActivity();
+            switch (msg.what) {
+                case Globals.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothHandler2.STATE_CONNECTED:
+                            Log.i(Globals.TAG, " BT: Connected");
+                            //do something
+                            break;
+                        case BluetoothHandler2.STATE_CONNECTING:
+                            Log.i(Globals.TAG, " BT: Connecting");
+                            //do something
+                            break;
+                        case BluetoothHandler2.STATE_NONE:
+                            Log.i(Globals.TAG, " BT: None");
+                            //do something
+                            break;
+                        case BluetoothHandler2.STATE_WAITING:
+                            Log.i(Globals.TAG, " BT: Waiting");
+                            //do something
+                            break;
+                    }
+                    break;
+                case Globals.DEVICE_NAME_IDENTIFIER:
+                    if (fragmentActivity != null) {
+                        Toast.makeText(fragmentActivity,
+                                "Connected to " + msg.getData().getString(Globals.DEVICE_NAME),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Globals.MESSAGE_READ:
+                    byte[] buffer = (byte[]) msg.obj;
+                    String incoming = new String(buffer, 0, msg.arg1);
+                    Toast.makeText(fragmentActivity, "Incoming: " + incoming, Toast.LENGTH_SHORT).show();
+                    break;
+                case Globals.MESSAGE_WRITE:
+                    byte[] buffer2 = (byte[]) msg.obj;
+                    String outgoing = new String(buffer2);
+                    Toast.makeText(fragmentActivity, "Outgoing: " + outgoing, Toast.LENGTH_SHORT).show();
+                    break;
+                case Globals.MESSAGE_TOAST:
+                    if (fragmentActivity != null) {
+                        Toast.makeText(fragmentActivity, msg.getData().getString(Globals.TOAST), Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get local Bluetooth adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
         // If the adapter is null, then Bluetooth is not supported
         if (mBluetoothAdapter == null) {
             Toast.makeText(getActivity(), "Bluetooth is not available", Toast.LENGTH_LONG).show();
@@ -78,101 +127,118 @@ public class SendFileFragment extends Fragment {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-            // Otherwise, setup the chat session
+        } else if (bluetoothHandler == null) {
+            setUp();
+            //todo
         }
     }
+
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (bHandler != null) {
-            //bHandler.stop);
+        if (bluetoothHandler != null) {
+            bluetoothHandler.stop();
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
-
-        // Performing this check in onResume() covers the case in which BT was
-        // not enabled during onStart(), so we were paused to enable it...
-        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
-//        if (bHandler != null) {
-//            // Only if the state is STATE_NONE, do we know that we haven't started already
-//            if (bHandler.getState() == BluetoothHandler.STATE_NONE) {
-//                // Start the Bluetooth services
-//                bHandler.start();
-//            }
-//        }
+        if (bluetoothHandler != null) {
+            if (bluetoothHandler.getStatus() == BluetoothHandler2.STATE_NONE) {
+                bluetoothHandler.start();
+            }
+        }
     }
 
     private void connectDevice(String address) {
-        bHandler.connectAsClient(mBluetoothAdapter.getRemoteDevice(address));
+        bluetoothHandler.connectAsClient(mBluetoothAdapter.getRemoteDevice(address));
+    }
+
+    private void connectDevice(Intent data) {
+        String address = data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        bluetoothHandler.connect(mBluetoothAdapter.getRemoteDevice(address));
+    }
+
+    private void makeDeviceDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 360);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    private void setUp() {
+        this.listDevices = new ArrayList<>();
+
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+        listDevicesView.setAdapter(adapter);
+
+        this.bluetoothHandler = new BluetoothHandler2(mBluetoothAdapter, messageHandler);
+
+        listDevicesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(Globals.TAG, " " + adapter.getItem(position));
+                //connectDevice(listDevices.get(position).getAddress());
+                Intent enableIntent = new Intent(listDevices.get(position).getAddress());
+                startActivityForResult(enableIntent, REQUEST_CONNECTION);
+
+
+               // Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
+                //startActivityForResult(serverIntent, REQUEST_CONNECTION);
+            }
+        });
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
+                //startActivityForResult(serverIntent, REQUEST_CONNECTION);
+                bluetoothHandler.write(" dette er en test! ".getBytes());
+            }
+        });
+
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.clear();
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                getActivity().registerReceiver(mReceiver, filter);
+                mBluetoothAdapter.startDiscovery();
+                makeDeviceDiscoverable();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    setUp();
+                } else {
+                    Toast.makeText(getActivity(), "BT not enabled", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case REQUEST_CONNECTION:
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data);
+                }
+        }
     }
 
      @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-         final View rootview = inflater.inflate(R.layout.fragment_send, container, false);
-         listDevicesView = (ListView) rootview.findViewById(R.id.list_devices);
-         btn_search = (Button) rootview.findViewById(R.id.btn_search);
-         btn_send = (Button) rootview.findViewById(R.id.btn_send);
-
-         this.listDevices = new ArrayList<>();
-
-         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
-         listDevicesView.setAdapter(adapter);
-
-         bHandler = new BluetoothHandler2(mBluetoothAdapter);
-
-
-         listDevicesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 //Log.d(Globals.TAG, listDevices.get(position).getAddress());
-//                 connectDevice(listDevices.get(position).getAddress());
-                 Log.d(Globals.TAG, " " + adapter.getItem(position));
-                 connectDevice(listDevices.get(position).getAddress());
-             }
-         });
-
-         btn_send.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-
-             }
-         });
-
-
-         btn_search.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 adapter.clear();
-                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                 getActivity().registerReceiver(mReceiver, filter);
-                 mBluetoothAdapter.startDiscovery();
-
-//                 pairedDevices = mBluetoothAdapter.getBondedDevices();
-//                 for (BluetoothDevice device : pairedDevices) {
-//                     adapter.add(device.getName() + "\n" + device.getAddress());
-//                 }
-//
-//                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//                // if (isChecked) {
-//                     adapter.clear();
-//                    listDevices.clear();
-//                     getActivity().registerReceiver(mReceiver, filter);
-//                     mBluetoothAdapter.startDiscovery();
-//                   //  isChecked = false;
-//                 //} else {
-//                   //  getActivity().unregisterReceiver(mReceiver);
-//                   //  mBluetoothAdapter.cancelDiscovery();
-//                  //   isChecked = true;
-//               //  }
-//                 listDevicesView.setAdapter(adapter);
-             }
-         });
-         return rootview;
+         return inflater.inflate(R.layout.fragment_send, container, false);
      }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        listDevicesView = (ListView) view.findViewById(R.id.list_devices);
+        btn_search = (Button) view.findViewById(R.id.btn_search);
+        btn_send = (Button) view.findViewById(R.id.btn_send);
+    }
 }
--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/dataclasses/Globals.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/dataclasses/Globals.java
@@ -22,4 +22,16 @@ public class Globals {
     public static final int MYLIST_BOOKMARKS  = 0;
     public static final int MYLIST_ALL        = -1;
 
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final int DEVICE_NAME_IDENTIFIER = 6;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
 }
