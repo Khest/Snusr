aa1125069a0c7a770aa41dc215ab5972baba7b0e
Khest <knut.j.hesten@student.hbv.no>
2016-05-31 14:38:43 +0200

    Reversed name of sorting by newest to oldest to oldest to newest

--- snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/dataclasses/Sorting.java
+++ snusr/src/main/java/no/hbv/gruppe1/snusr/snusr/dataclasses/Sorting.java
@@ -15,7 +15,7 @@ public enum  Sorting {
     NICOTINELEVEL("Nikotinnivå", DatabaseHelper.FeedEntry.col_snus_nicotinelevel),
     TYPE("Type", DatabaseHelper.FeedEntry.col_snus_type),
     STRENGTH("Styrke", DatabaseHelper.FeedEntry.col_snus_strength),
-    NEWESTTOOLDEST("Nyeste til gamleste", DatabaseHelper.FeedEntry.col_snus_id);
+    OLDESTTONEWEST("Nyeste til gamleste", DatabaseHelper.FeedEntry.col_snus_id);
 
 
     private String guiName, columnName;
